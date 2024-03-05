875d37ae3b701704eecf5e4d9d50700b825e4b65
maeci <cimafrancamae@gmail.com>
2024-03-06 04:00:39 +0800

    Add categories controller and views

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,51 @@
 class CategoriesController < ApplicationController
+  before_action :set_category, only: %i[show edit update destroy]
+  before_action :authenticate_user!
 
   def index
-
+    @categories = current_user.categories
   end
+
+  def show; end
+
+  def create
+    puts category_params
+    @category = current_user.categories.build(category_params)
+
+    if @category.save
+      redirect_to categories_path, notice: "Category saved successfully."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def edit; end
+
+  def update
+    if @category.update(category_params)
+      redirect_to :categories, notice: "Category updated successfully."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @category.destroy
+    redirect_to @categories_path, notice: "Category deleted successfully."
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
+
+  def set_category
+    @category = current_user.categories.find(params[:id])
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_05_072559) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_05_191247) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,6 +18,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_05_072559) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_categories_on_user_id"
   end
 
   create_table "tasks", force: :cascade do |t|
@@ -43,5 +45,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_05_072559) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "categories", "users"
   add_foreign_key "tasks", "categories"
 end
